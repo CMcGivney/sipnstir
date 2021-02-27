08dc3e4a5bef1a896f2db5bc9f4e54596a15c1bc
Chris McGivney <cmmcgivney84@gmail.com>
2021-02-27 14:02:33 -0700

    work: worked to use cloudinary api, still needs fine css tuning

--- src/pages/GalleryAPI.js
+++ src/pages/GalleryAPI.js
@@ -1,17 +1,47 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { CloudinaryContext, Transformation, Image } from "cloudinary-react";
-
+import NavBar from "../components/NavBar";
 const GalleryApi = () => {
-  const [cloudImages, setCloudImages] = useState([]);
+  const [cocktailImages, setCocktailImages] = useState([]);
+  const [wineImages, setWineImages] = useState([]);
+  const [foodImages, setFoodImages] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get("https://res.cloudinary.com/cmmc/image/list/wine.json")
+        .then((res) => {
+          console.log(res.data.resources);
+          setWineImages(res.data.resources);
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    };
+    fetchData();
+  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get("https://res.cloudinary.com/cmmc/image/list/food.json")
+        .then((res) => {
+          console.log(res.data.resources);
+          setFoodImages(res.data.resources);
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    };
+    fetchData();
+  }, []);
   useEffect(() => {
     const fetchData = async () => {
       await axios
         .get("https://res.cloudinary.com/cmmc/image/list/cocktail.json")
         .then((res) => {
           console.log(res.data.resources);
-          setCloudImages(res.data.resources);
+          setCocktailImages(res.data.resources);
         })
         .catch((error) => {
           console.log(error.response);
@@ -21,26 +51,60 @@ const GalleryApi = () => {
   }, []);
 
   return (
-    <div className="galleryImgContainer">
+    <>
+      <NavBar />
       <CloudinaryContext cloudName="cmmc">
-        {cloudImages
-          ? cloudImages.map((data) => {
-              return (
-                <Image
-                  className="galleryImages"
-                  publicId={data.public_id}
-                  key={data.public_id}
-                >
-                  {data.public_id ===
-                  "sipnstir/pink_mint_lemon_cocktail_s9xjx8" ? (
-                    <Transformation angle="18" background="grey" />
-                  ) : null}
-                </Image>
-              );
-            })
-          : null}
+        <div className="galleryImgContainer">
+          {cocktailImages
+            ? cocktailImages.map((data) => {
+                return (
+                  <Image
+                    className="galleryImages"
+                    publicId={data.public_id}
+                    key={data.public_id}
+                    width="150"
+                    height="150"
+                  ></Image>
+                );
+              })
+            : null}
+        </div>
       </CloudinaryContext>
-    </div>
+      <CloudinaryContext cloudName="cmmc">
+        <div className="galleryImgContainer">
+          {foodImages
+            ? foodImages.map((data) => {
+                return (
+                  <Image
+                    className="galleryImages"
+                    publicId={data.public_id}
+                    key={data.public_id}
+                    width="150"
+                    height="150"
+                  ></Image>
+                );
+              })
+            : null}
+        </div>
+      </CloudinaryContext>
+      <CloudinaryContext cloudName="cmmc">
+        <div className="galleryImgContainer">
+          {wineImages
+            ? wineImages.map((data) => {
+                return (
+                  <Image
+                    className="galleryImages"
+                    publicId={data.public_id}
+                    key={data.public_id}
+                    width="150"
+                    height="150"
+                  ></Image>
+                );
+              })
+            : null}
+        </div>
+      </CloudinaryContext>
+    </>
   );
 };
 
